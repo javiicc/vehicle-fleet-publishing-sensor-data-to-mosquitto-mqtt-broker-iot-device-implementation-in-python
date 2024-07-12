77c6663f19387ae294a2905347df8fab6c813f52
Javier <82763996+javiicc@users.noreply.github.com>
2024-07-12 16:53:05 +0100

    feat: add gps

--- mqtt-vehicle-fleet/iot/sensors.py
+++ mqtt-vehicle-fleet/iot/sensors.py
@@ -2,10 +2,12 @@ from abc import ABC, abstractmethod
 from uuid import uuid4
 from random import uniform
 import time
+import pandas as pd
+import os
 
 
 class Sensor(ABC):
-    def __init__(self, id: str):
+    def __init__(self, id: str) -> None:
         self.id = id
 
     @abstractmethod
@@ -14,12 +16,12 @@ class Sensor(ABC):
 
 
 class Thermistor(Sensor):
-    def __init__(self, location):
+    def __init__(self, location) -> None:
         super().__init__(str(uuid4()))
         self.location = location
         self.unit = "Celsius"
         
-    def read(self):
+    def read(self) -> dict:
         return {
             "id": self.id,
             "timestamp": time.time(),
@@ -27,8 +29,31 @@ class Thermistor(Sensor):
             "unit": self.unit,
             "location": self.location
         }
+    
 
+class GPS(Sensor):
+    def __init__(self, route: str) -> None:
+        super().__init__(str(uuid4()))
+        self.file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "coordinates", route + ".csv")
+        self.coords = pd.read_csv(self.file_path)
+        self.last_coords = None
+
+    def read(self) -> dict:
+        current_coords = self.coords.iloc[0].values
+        self.last_coords = current_coords
+
+        return {
+            "id": self.id,
+            "timestamp": time.time(),
+            "lat": current_coords[0].item(),
+            "lon": current_coords[1].item()
+        }
+    
 
 if __name__ == "__main__":
-    thermistor = Thermistor("engine")
-    print(thermistor.read())
+
+    # thermistor = Thermistor("engine")
+    # print(thermistor.read())
+
+    gps = GPS("dublin-limerick")
+    print(gps.read())
